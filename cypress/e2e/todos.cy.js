783cd91758dbfeb17a37ed88f6fb5d2201ff1b05
SomeArtem <serebryakov.artemiy@mail.ru>
2023-04-22 21:16:34 +0300

    '200423_finished_first_test_goes_fine

--- cypress/e2e/todos.cy.js
+++ cypress/e2e/todos.cy.js
@@ -6,55 +6,54 @@ const SERVER_URL='http://127.0.0.1:5173/';
 describe('test todo page', () => {
     beforeEach(() => {
         //cy.intercept('/**/*.js').as('getTaskPopup');
-        //cy.intercept('src/**/*.js').as('jsDelivered');
-        cy.intercept('src/**/popup/*.*').as('popup');
+        cy.intercept('src/**/*.js').as('js-file-request');
+        //cy.intercept('src/**/popup/*.*').as('popupDelivered');
     });
 
-    it.only('user open main page', () => {        
+    it('user open main page', () => {
+        //my super great test      
         cy.visit(SERVER_URL);
-        cy.wait(200);
+        cy.wait(100);
         cy.url().should('include', SERVER_URL);    
-        cy.get(`#${DOM.Button.CREATE_TASK}`,{timeout:300}).should('exist');
-        // cy.wait(100);      
-        cy.get(`#${DOM.Button.CREATE_TASK}`).click();
+        cy.get(`#${DOM.Button.CREATE_TASK}`,{timeout:300}).should('exist').click();
         cy.get(`#${DOM.Popup.CONTAINER}`).should('not.have.class','hidden');
     });
 
-
-    it('user open main page', () => {
-        //этот заебись проходит
+    it.only('vladimir shit off his pants', () => {
         cy.visit(SERVER_URL);
-        cy.url().should('include', SERVER_URL)
-        
-        cy.get(`#${DOM.Button.CREATE_TASK}`).click();
-        cy.wait('@getTaskPopup');       
-        // cy.wait(300);
+        cy.url().should('include', SERVER_URL);
 
-        cy.get(`#${DOM.Button.CREATE_TASK}`).should('exist').should('contain.text','+ Create Task').should('have.length.gt',0);
-
+        cy.get(`#${DOM.Popup.CONTAINER}`).should('exist').should('have.class','hidden').find('.spinner').should('exist');
+        cy.get(`#${DOM.Button.CREATE_TASK}`,{timeout:300}).should('exist').click().click();
         cy.get(`#${DOM.Popup.CONTAINER}`).should('exist').should('not.have.class','hidden');
-
-
-
-         
-    });
-
-    it('user open main page', () => {
-        beforeEach(() => {
-            cy.intercept('/src/view/**',)
-        });
-
-        cy.visit('http://127.0.0.1:5173/');
-        cy.get(`#${DOM.Button.CREATE_TASK}`)
-        .should('exist')
-        .should('contain.text','+ Create Task')
-        .should('have.length.gt',0)
-        .click();
-
         
-        cy.visit('http://127.0.0.1:5173/');
-        cy.get(`#${DOM.Button.CREATE_TASK}`).wait().then(()=>{
-            cy.get(`#${DOM.Popup.CreateTask.TITLE}`).should('contain.text','')
-        });
+        const popupTask=cy.get('[data-testId="task-popup"]');
+        const todoTaskText='Welcome task 2';
+
+        popupTask
+            .should('exist')
+            .should('be.visible');
+
+        popupTask
+            .find('[data-id="inpTitle"]')
+            .should('exist')
+            // .its('value').should('be.empty')
+            .should('have.value','')
+            .type(todoTaskText);
+
+        cy.get('[data-testId="task-popup"]')
+            .find('[data-id="btnConfirm"]')
+            .should('exist')
+            .should('contain.text','Create')
+            .click();
+
+        cy.get('[data-testId="tasksColumn"]')
+            .should('exist')
+            .children()
+            .should('have.length', 2)
+            .first()
+            .find('[data-id="templateTaskTitle"]')
+            .should('contain.text',todoTaskText);
+
     });
 });
