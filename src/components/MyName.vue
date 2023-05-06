16f347bc049d4a51cb1718439343caa31b32fa1e
SomeArtem <Serebryakov.artemiy@mail.ru>
2023-05-06 13:40:18 +0300

    sdgadsg

---
+++ src/components/MyName.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    Enter name:
+    <input type="text" v-model="firstName" />
+  </h1>
+  <h1>{{ firstName }}</h1>
+</template>
+
+<script>
+export default {
+  name: "MyName",
+  data() {
+    return {
+      firstName: "af",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
