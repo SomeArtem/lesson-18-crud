7b17c7093d93912a31014926d69d7703fca85819
SomeArtem <Serebryakov.artemiy@mail.ru>
2023-05-13 11:20:30 +0300

    watcher

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,64 @@
 <script setup>
 //import HelloWorld from "./components/HelloWorld.vue";
 import MyName from "./components/MyName.vue";
+import TodoItem from "./components/TodoItem.vue";
 </script>
 
 <template>
   <h3>To do list</h3>
-  <input v-model="inputText" type="text" @keyup.enter="onKeyupEnter">
+  <input v-model="inputText" type="text" @keyup.enter="onKeyupEnter" />
   <div>
     List:
-    <div v-for="todo in todos" :key="todo">{{todo}}</div>
+    <div>
+      <TodoItem
+        v-for="(item, index) in todos"
+        :key="item"
+        :text="item"
+        :index="index + 1"
+        @delete="deleteMethod(index)"
+      />
+    </div>
   </div>
 </template>
 
 
 <script>
-  export default{
-    data: ()=>({
-      inputText:'',
-      todos:[],
-    }),
-    computed:{
-      canAddItemToList(){
-        return this.inputText>0;
-      }
+const LOCAL_KEY_TODOS = "todos";
+export default {
+  data: () => ({
+    inputText: "",
+    todos: [],
+  }),
+  computed: {
+    canAddItemToList() {
+      return this.inputText > 0;
     },
-    methods:{
-      onKeyupEnter(){
-        this.todos.push(this.inputText);
-        this.inputText='';
-      }
+  },
+  methods: {
+    onKeyupEnter() {
+      this.todos.push(this.inputText);
+      this.inputText = "";
+    },
+    deleteMethod(el) {
+      console.log("delete", el);
+      this.todos.splice(el, 1);
+    },
+  },
+  created() {
+    const rawTodos = localStorage.getItem(LOCAL_KEY_TODOS);
+    console.log("created");
+    if (rawTodos) {
+      this.todos = JSON.parse(rawTodos);
     }
-  }
+    this.$watch(
+      () => this.todos,
+      (newValue, prevValue) => {
+        console.log(newValue, prevValue);
+      }
+    );
+  },
+};
 </script>
 
 <style scoped>
-/* .logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-} */
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <span>{{ index }}. {{ text }}</span>
+    <button @click="$emit('delete')">delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    index: {
+      type: Number,
+      default: 0,
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+  emits: ["delete"],
+};
+</script>
+
+<style>
+</style>
