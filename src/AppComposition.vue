33c624d770ba3b185013862c6648ae6bcbc41ef4
SomeArtem <serebryakov.artemiy@mail.ru>
2023-05-25 00:02:33 +0300

    handmade navigation works

--- src/AppComposition.vue
+++ src/AppComposition.vue
@@ -1,132 +1,45 @@
 <script setup>
-import { computed, onMounted, ref, watch } from 'vue';
+import {onMounted, ref} from 'vue';
 import AppHeader from './components/AppHeader.vue';
 import MyName from "./components/MyName.vue";
-import TodoItem from "./components/TodoItem.vue";
+import TodoPage from './components/TodoPage.vue';
 
-const LOCAL_KEY_TODOS = "todos";
-const LOCAL_KEY_INP_TEXT = "input_text";
+const PAGES=['index', 'todos'];
 
-const inputText = ref(JSON.parse(localStorage.getItem(LOCAL_KEY_INP_TEXT)||null));
-const todos = ref(JSON.parse(localStorage.getItem(LOCAL_KEY_TODOS)||'[]'));
-const canAddItemToList=computed(()=>true);
-const gettodotext=computed(()=> inputText.value?.trim());
-const gettodocount=computed(()=> todos.value.length>5 ? 'МНОГА' : todos.value.length);
-const onKeyupEnter =()=>{
-  console.log('onKeyupEnter',gettodotext.value);
-  todos.value.push(gettodotext.value);
-  inputText.value = "";
+const user=ref({name:"Artom"});
+const currentPage=ref(PAGES[0]);
+
+const onChangePage=(pageName)=>{
+  console.log('onChangePage', pageName);
+  currentPage.value=pageName;
 }
-
-const deleteMethod =(ind)=>{
-  console.log("deleteMethod", ind);
-  todos.value.splice(ind, 1);
-}
-
-watch(inputText, (value)=>{
-        console.log(value);
-        localStorage.setItem(LOCAL_KEY_INP_TEXT, JSON.stringify(value));
-});
-
-watch(todos, (value) => {
-    console.log('todoStopWatch',value);
-    localStorage.setItem(LOCAL_KEY_TODOS, JSON.stringify(value));
-  }, {deep:true});
 
 onMounted(()=>{
-  console.log('onMounted')
+  console.log('onMounted');
 });
-
 </script>
 
 <template>
-  <!-- <AppHeader>
+  <AppHeader>
     Todo App
     <template #sub-header >
       <span v-if="user">created by {{user.name}}</span>
       <span v-else>noname</span>
     </template>
-  </AppHeader>   -->
-  <input ref="dominput" v-model="inputText" type="text" @keyup.enter="canAddItemToList && onKeyupEnter()" />
+  </AppHeader> 
+
   <div>
-    List: <span v-if="todos.length">({{gettodocount}})</span>
-    <span v-else>empty</span>
-    <div>
-      <TodoItem
-        v-for="(item, index) in todos"
-        :key="item"
-        :text="item"
-        :index="index + 1"
-        @delete="deleteMethod(index)"
-      />
-    </div>
+    <div>Menu</div>
+    <span><button @click="onChangePage(PAGES[0])">index</button></span>
+    <span><button @click="onChangePage(PAGES[1])">todos</button></span>
+  </div>
+
+  <TodoPage v-if="user && currentPage===PAGES[1]"/>
+  <div v-if="currentPage===PAGES[0]">
+    HOME PAGE
   </div>
 </template>
 
-
-<script>
-
-
-let todoStopWatch;
-
-export default {
-  components:{}
-//   data: () => ({
-//     inputText: "",
-//     todos: [],
-//     user:{name:'Ortem'},
-//   }),
-//   computed: {
-//     canAddItemToList() {
-//       return this.todotext.length > 0;
-//     },
-//     todotext(){
-//       return this.inputText.trim();
-//     },
-//     gettodocount(){return this.todos.length>5 ? 'МНОГА' : this.todos.length}
-//   },
-//   methods: {
-//     onKeyupEnter() {
-//       this.todos.push(this.todotext);
-//       this.inputText = "";
-//       console.log('asdasdasd')
-//     },
-//     deleteMethod(ind) {
-//       console.log("delete", ind);
-//       this.todos.splice(ind, 1);
-
-//     },
-//   },
-//   created() {
-//     const rawTodos = localStorage.getItem(LOCAL_KEY_TODOS);
-//     console.log("created rawTodos: ", rawTodos);
-//     this.todos=rawTodos && JSON.parse(rawTodos) || [];
-//     this.inputText = JSON.parse(localStorage.getItem(LOCAL_KEY_INP_TEXT)||null);
-    
-//     todoStopWatch = this.$watch(
-//       () => this.todos,
-//       (value) => {
-//         console.log(value);
-//         localStorage.setItem(LOCAL_KEY_TODOS, JSON.stringify(value));
-//       }, {deep:true}
-//     );
-//     this.$watch(
-//       () => this.inputText,
-//       (value) => {
-//         console.log(value);
-//         localStorage.setItem(LOCAL_KEY_INP_TEXT, JSON.stringify(value));
-//       },);
-//   },
-
-//   mounted(){
-//     console.log('mounted, dominput: ', this.$refs.dominput)
-//   },
-
-//   unmounted(){
-//     todoStopWatch();
-//   }
-};
-</script>
-
 <style scoped>
 </style>
+TodoPage
---
+++ src/components/TodoPage.vue
@@ -0,0 +1,67 @@
+<script setup>
+    import { computed, onMounted, ref, watch } from 'vue';
+    import TodoItem from "./TodoItem.vue";
+    import {parseLocalStorage, saveToLocalStorage} from '../utils/StorageUtils'
+    
+    const LOCAL_KEY_TODOS = "todos";
+    const LOCAL_KEY_INP_TEXT = "input_text";
+
+
+    const inputText = ref(parseLocalStorage(LOCAL_KEY_INP_TEXT, null));
+    const todos = ref(parseLocalStorage(LOCAL_KEY_TODOS,'[]'));
+   
+
+    const canAddItemToList=computed(()=>true);
+    const gettodotext=computed(()=> inputText.value?.trim());
+    const gettodocount=computed(()=> todos.value.length>5 ? 'МНОГА' : todos.value.length);
+    const onKeyupEnter =()=>{
+    console.log('onKeyupEnter',gettodotext.value);
+    todos.value.push(gettodotext.value);
+    inputText.value = "";
+    }
+
+    const deleteMethod =(ind)=>{
+    console.log("deleteMethod", ind);
+    todos.value.splice(ind, 1);
+    }
+
+    watch(inputText, (value)=>{
+            console.log(value);
+            saveToLocalStorage(LOCAL_KEY_INP_TEXT, value);
+    });
+
+    watch(todos, (value) => {
+        console.log('todoStopWatch',value);
+        saveToLocalStorage(LOCAL_KEY_TODOS, value);
+    }, {deep:true});
+</script>
+
+
+
+<template>
+    <input ref="dominput" v-model="inputText" type="text" @keyup.enter="canAddItemToList && onKeyupEnter()" />
+    <div>
+        List: <span v-if="todos.length">({{gettodocount}})</span>
+        <span v-else>empty</span>
+        <div>
+        <TodoItem
+            v-for="(item, index) in todos"
+            :key="item"
+            :text="item"
+            :index="index + 1"
+            @delete="deleteMethod(index)"
+        />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TodoPage",
+
+}
+</script>
+
+<style scoped>
+
+</style>
