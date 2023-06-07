ea0f14cb74bfd39dd9396c5456d897023a4a2d04
SomeArtem <serebryakov.artemiy@mail.ru>
2023-06-07 23:50:08 +0300

    lesson37 done, installed couchdb, works fine. run as a service, autorun disabled

--- src/main.js
+++ src/main.js
@@ -16,6 +16,34 @@ import PocketBase from 'pocketbase';
 
 const pb = new PocketBase(import.meta.env.VITE_SERVER_PATH);
 
+let db=new PouchDB('http://127.0.0.1:5984/books');
+// console.log('PouchDB', db);
+// console.log('db.get', db.get('Book1'));
+db.get('Book1').then((doc)=>{
+    console.log(doc);
+}).catch((err)=>{
+    console.log('err:', err);
+});
+
+
+
+db.changes({
+    since: 'now',
+    live: true,
+    include_docs: true
+  }).on('change', function(change) {
+    console.log('change', change);
+    // handle change
+  }).on('complete', function(info) {
+    console.log('complete', info);
+    // changes() was canceled
+  }).on('error', function (err) {
+    console.log(err);
+  });
+
+
+
+
 console.log("authStore.isValid ", pb.authStore.isValid);
   
 
